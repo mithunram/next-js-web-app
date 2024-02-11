68d504a82dbf965817238ba588174d622f446437
Mithunram <mithunram.vp@gmail.com>
2024-02-11 12:37:18 -0500

    Update page.tsx

--- src/app/(root)/(app)/(home)/page.tsx
+++ src/app/(root)/(app)/(home)/page.tsx
@@ -7,14 +7,14 @@ export default function HomePage() {
       <div className="relative flex items-center justify-center py-3">
         <h1 className="relative flex-1">
           <span className="grid grid-cols-2">
-            <span className="block text-center text-sm opacity-90">I AM</span>
+            <span className="block text-center text-sm opacity-90">Welcome to</span>
           </span>{' '}
           <span className=" z-10 block text-[calc(1.825rem+6.9vw)] font-bold leading-none">Dede Ard</span>{' '}
           <span className="grid grid-cols-2 justify-items-end">
             <span className="block"></span>
             <span className="block text-sm opacity-90">
-              A FULL STACK
-              <br /> WEB DEVELOPER
+              Earthy
+              <br /> Designs
             </span>
           </span>
         </h1>
